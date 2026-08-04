da59442390bfff14c950650fa3ed4969dbffb01a
Silas Felipe <devsilasfelipe@gmail.com>
2026-08-04 08:29:49 -0300

    feat: add leap year check for date validation

--- src/date.c
+++ src/date.c
@@ -1,5 +1,21 @@
 #include "date.h"
 
+static int bissexto(long year){
+    if (year % 4 == 0 && (
+        year % 100 != 0 || 
+        year % 400 == 0)){
+            return 1;
+        }
+    return 0;
+}
+
 int dateValid(Date data){
-    
+    if (data.second < 0   || data.second > 59) return 0;
+    if (data.minute < 0   || data.minute > 59) return 0;
+    if (data.hour   < 0   || data.hour   > 23) return 0;
+    if (data.day    < 1   || data.day    > 31) return 0;
+    if (data.month  < 1   || data.month  > 12) return 0;
+    if (data.year   < 1) return 0;
+
+    return 1;
 }
